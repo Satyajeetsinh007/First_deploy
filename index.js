634a86077acf732c338eace26c7ea72803d96ed8
Satyajeetsinh007 <satyajadeja52@gmail.com>
2026-09-17 19:03:19 +0530

    added a demo app

--- index.js
+++ index.js
@@ -5,4 +5,9 @@ app.get('/', function (req, res) {
     res.set("content-type", "text/plain");
     res.send("Hello world!");
 });
+
+app.get('/demo', (req, res) => {
+    res.set("content-type", "text/html");
+    res.send('<h1>Demo app</h1>')
+})
 app.listen(process.env.PORT);
